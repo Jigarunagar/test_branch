90f2e75f34c577320bad34970aad4e8415086523
Jigar Unagar <officialjigarunagar@gmail.com>
2025-10-05 21:44:15 +0530

    update

---
+++ client/sad.js
@@ -0,0 +1,14 @@
+console.log(`sad.js loaded`);
+export const sad = 'sad';
+export default sad;
+export const sad2 = 'sad2';
+export const sad3 = 'sad3';
+
+export function sadFn() {
+    console.log('sadFn called');    
+    return 'sadFn';
+}
+export function sadFn2() {
+    console.log('sadFn2 called');
+    return 'sadFn2';
+}
